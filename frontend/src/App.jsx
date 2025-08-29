2cc56b2e4b7d7cad41563659e8c5126cb1920089
Tushararya <tushararya773@gmail.com>
2025-08-29 22:26:32 +0530

    r

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import AyurvedaLanding from "./pages/AyurvedaLanding";
-import Login from "./pages/Login";
+
 
 
 const App = () => {
   return (
     <>
-      {/* <AyurvedaLanding />  */}
-      <Login />
+      <AyurvedaLanding /> 
+      
     </>  
   );
 }
